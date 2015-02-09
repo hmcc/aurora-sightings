e535150b29721008616ad66814a384d3d369bbbf
hmcc <heather@problemchimp.org>
2015-02-09 21:24:42 +0000

    Removed unnecessary @EnableAutoConfiguration

--- src/main/java/org/aurorasightings/Application.java
+++ src/main/java/org/aurorasightings/Application.java
@@ -2,14 +2,12 @@ package org.aurorasightings;
 
 import org.aurorasightings.task.Search;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableAutoConfiguration
 @EnableScheduling
 @ComponentScan
 public class Application extends SpringBootServletInitializer {
